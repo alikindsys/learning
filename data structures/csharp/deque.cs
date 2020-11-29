fa3722cd180d28c24e5414f6f29788c704260fbd
roridev <t3ctotalmenterandom1@outlook.com>
2020-11-29 16:58:04 -0300

    Adds basic fields

--- data structures/csharp/deque.cs
+++ data structures/csharp/deque.cs
@@ -1,5 +1,10 @@
+using System;
+
 namespace learning.data_structures {
     public class Deque<T> {
-
+        private T[] data = Array.Empty<T>();
+        public int Capacity;
+        public int IndexFirst;
+        public int IndexLast;
     }
 }

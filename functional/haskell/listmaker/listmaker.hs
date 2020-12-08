fc6da83656b71dbd9a922f23f212ec93eea96141
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-08 14:02:41 -0300

    Removes legacy code

--- functional/haskell/listmaker/listmaker.hs
+++ functional/haskell/listmaker/listmaker.hs
@@ -68,34 +68,20 @@ tokenize [i] buffer
   | i == '(' = LParen : Item (reverse buffer) : []
   | otherwise = Item (reverse $ i:buffer) : []
 
+---
+--- Parsing
+--- It is required for parsing to create an Abstract Syntax Tree
+--- Said structure is still not implemented and more reserach is
+--- required in order to implement it, including a lot of fights
+--- with Haskell.
+---
+
+-- parse :: [Token] -> AST
+
+
+---
+--- Helper Functions
+---
 
 isNum :: String -> Bool
 isNum x = all isDigit x
-
-formalize :: String -> [String]
-formalize [] = []
-formalize s = splitStuff ',' s
-
-isCategory :: String -> Bool
-isCategory x =  '(' `elem` x
-
-parse :: Int -> [String] -> [Maybe (Either Category ItemStack)]
-parse _ _ = []
-
-parseSimple :: String -> Maybe ItemStack
-parseSimple x
-  | checkStart && checkEnd = Just $ ItemStack (head $ splitStuff '(' x) (read (replace ')' ' ' . head $ getInnerData x) :: Int) 
-  | checkStart || checkEnd = Nothing
-  | otherwise = Just $ ItemStack x 0 
-  where checkStart = '(' `elem` x
-        checkEnd = ')' `elem` x
-
-getInnerData :: String -> [String]
-getInnerData x = filter (\s -> ')' `elem` s) $ splitStuff '(' x
-
-splitStuff :: Eq a => a -> [a] -> [[a]]
-splitStuff d [] = []
-splitStuff d s = x : splitStuff d (drop  1 y) where (x,y) = span (/= d) s
-
-replace :: Eq a => a -> a -> [a] -> [a]
-replace a b = map $ \c -> if c == a then b else c

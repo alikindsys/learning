a389330b85520b6bc74fcf7415566a8487a7b8dd
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-12 22:36:04 -0300

    Implements the parser! FINALLY.

--- functional/haskell/listmaker/listmaker.hs
+++ functional/haskell/listmaker/listmaker.hs
@@ -9,15 +9,6 @@ import Data.Char
 -- depends a bit on portuguese syntax but could? be ported over to english
 
 --
--- Definitions:
--- Itemstack (an single entry, nullable, can contain quantity and aditional data)
--- Category (An arbitrary list of itemstacks under a common name, can contain a quantity)
---
-
-data ItemStack = ItemStack {item::String, quantity::Int} deriving (Show)
-data Category = Category String [ItemStack] Int
-
---
 -- About the input string and expected formatting
 -- Comma-separated. External data are inserted with parens.
 -- Ex : "item, item2 (3), category (item, item2, item), category (item, item2 4 of each)"
@@ -41,56 +32,71 @@ toTokens x = tokenize x ""
 tokenize :: String -> String -> [Token]
 
 tokenize [] [] = []
-tokenize [] a = Item a:[]
+tokenize [] a = [Item a]
 
 tokenize (i:j:rest) buffer
   | i == ',' = Comma: tokenize (j:rest) []
   | i == '(' = LParen: tokenize (j:rest) []
   | i == ')' = RParen: tokenize (j:rest) []
-  | j == ')' || j == '(' && isNum (i:buffer) = Number (read . reverse $ i:buffer :: Int):tokenize (j:rest) []
+  | (j == ')' || j == '(') && isNum (i:buffer) = Number (read . reverse $ i:buffer :: Int):tokenize (j:rest) []
   | j == ')' || j == '(' = Item (reverse $ i:buffer):tokenize (j:rest) []
-  | j == ',' = Item (reverse $ i:buffer):tokenize (j:rest) []
+  | j == ','  = Item (reverse $ i:buffer):tokenize (j:rest) []
   | otherwise = tokenize (j:rest) (i:buffer)
   
 tokenize [i] []
-  | i == ',' = Comma : []
-  | i == ')' = RParen : []
-  | i == '(' = LParen : []
-  | isDigit i = Number (read [i] :: Int) : []
-  | otherwise = Item [i] : []
+  | i == ','  = [Comma]
+  | i == ')'  = [RParen]
+  | i == '('  = [LParen]
+  | isDigit i = [Number (read [i] :: Int)]
+  | otherwise = [Item [i]]
 
 tokenize [i] buffer
-  | isNum buffer && i == ',' = Comma : Number (read buffer :: Int) : []
-  | isNum buffer && i == ')' = RParen : Number (read buffer :: Int) : []
-  | isNum buffer && i == '(' = LParen : Number (read buffer :: Int) : []
-  | i == ',' = Comma : Item (reverse buffer) : []
-  | i == ')' = RParen : Item (reverse buffer) : []
-  | i == '(' = LParen : Item (reverse buffer) : []
-  | otherwise = Item (reverse $ i:buffer) : []
+  | isNum buffer && i == ',' = [Comma, Number (read buffer :: Int)]
+  | isNum buffer && i == ')' = [RParen, Number (read buffer :: Int)]
+  | isNum buffer && i == '(' = [LParen, Number (read buffer :: Int)]
+  | i == ',' = [Comma, Item (reverse buffer)]
+  | i == ')' = [RParen, Item (reverse buffer)]
+  | i == '(' = [LParen, Item (reverse buffer)]
+  | otherwise = [Item (reverse $ i:buffer)]
 
----
---- Parsing
---- It is required for parsing to create an Abstract Syntax Tree
---- Said structure is still not implemented and more reserach is
---- required in order to implement it, including a lot of fights
---- with Haskell.
----
-
----
---- Grammar
----
-
---- itemstack     ::= item lparen number rparen | item
---- categoryInner ::= itemstack comma categoryInner | itemstack 
---- category      ::= item lparen category rparen | item lparen categoryInner rparen
+-- AST
+data AST = Single String
+      | Multiple String Int
+      | Category String [AST]
+      deriving Show
 
 
--- parse :: [Token] -> AST
+-- Parsing
+parseOne :: [Token] -> Maybe (AST,[Token])
+parseOne [] = Nothing
+parseOne (Comma : xs) = parseOne xs
 
+parseOne (Item i : LParen : Number n: RParen: xs) = Just (Multiple i n, xs)
 
+parseOne (Item name : LParen : xs) = Just (Category name contents, rest')
+  where (contents, rest) = parseMany xs
+        rest' = case rest of
+                     [RParen] -> []
+                     (RParen : nommed) -> nommed
+                     _ -> error "Unmatched Parenthesis"
+
+parseOne (Item x : xs) = Just (Single x, xs)
+parseOne _ = Nothing
+
+parseMany :: [Token] -> ([AST], [Token])
+parseMany [] = ([],[])
+parseMany tokens = case parseOne tokens of
+                        Just (item, rest) -> let (items, rest') = parseMany rest in (item : items, rest')
+                        Nothing -> ([], tokens)
 ---
 --- Helper Functions
 ---
 
+rest'' :: [Token] -> [Token]
+rest'' x = case x of
+               [RParen] -> []
+               (RParen : nommed) -> nommed
+               _ -> error "Unmatched Parenthesis"
+
 isNum :: String -> Bool
-isNum x = all isDigit x
+isNum = all isDigit
